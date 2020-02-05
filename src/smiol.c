18db875a3ae1b8bc075dce0c5acf0b6a330b348e
Michael Duda <duda@ucar.edu>
2020-02-05 16:38:32 -0700

    Add initial parallel-netCDF implementation of SMIOL_define_var
    As part of this commit, new variable type constants are define for
    32- and 64-bit real variables, 32-bit integer variables, and character
    variables.
    
    The unit test routines for SMIOL_define_var and SMIOLf_define_var
    are currently empty -- they simply create a context and a file.

--- src/smiol.c
+++ src/smiol.c
@@ -434,12 +434,24 @@ int SMIOL_inquire_dim(struct SMIOL_file *file, const char *dimname, SMIOL_Offset
  *
  * Defines a new variable in a file.
  *
- * Detailed description.
+ * Defines a variable with the specified name, type, and dimensions in an open
+ * file pointed to by the file argument. The varname and dimnames arguments
+ * are expected to be null-terminated strings.
+ *
+ * Upon successful completion, SMIOL_SUCCESS is returned; otherwise, an error
+ * code is returned.
  *
  ********************************************************************************/
 int SMIOL_define_var(struct SMIOL_file *file, const char *varname, int vartype, int ndims, const char **dimnames)
 {
-int i;
+#ifdef SMIOL_PNETCDF
+	int *dimids;
+	int ierr;
+	int i;
+	nc_type xtype;
+	int varidp;
+#endif
+
 	/*
 	 * Check that file handle is valid
 	 */
@@ -454,22 +466,68 @@ int i;
 		return SMIOL_INVALID_ARGUMENT;
 	}
 
-if (vartype != 0) {
-	return SMIOL_INVALID_ARGUMENT;
-}
+	/*
+	 * Check that the variable type is valid - handled below in a library-specific way...
+	 */
 
 	/*
 	 * Check that variable dimension names are valid
 	 */
-	if (dimnames == NULL && ndims > 0) {
+	if (dimnames == NULL && ndims >= 0) {
 		return SMIOL_INVALID_ARGUMENT;
 	}
 
-fprintf(stderr, "> Defining variable %s\n", varname);
-fprintf(stderr, "> Variable type is %i\n", vartype);
-for (i=0; i<ndims; i++) {
-	fprintf(stderr, "> Dimension %i is %s\n", i, dimnames[i]);
-}
+#ifdef SMIOL_PNETCDF
+	dimids = (int *)malloc(sizeof(int) * (size_t)ndims);
+	if (dimids == NULL) {
+		return SMIOL_MALLOC_FAILURE;
+	}
+
+	/*
+	 * Build a list of dimension IDs
+	 */
+	for (i=0; i<ndims; i++) {
+		if ((ierr = ncmpi_inq_dimid(file->ncidp, dimnames[i], &dimids[i])) != NC_NOERR) {
+			free(dimids);
+			file->context->lib_type = SMIOL_LIBRARY_PNETCDF;
+			file->context->lib_ierr = ierr;
+			return SMIOL_LIBRARY_ERROR;
+		}
+	}
+
+	/*
+	 * Translate SMIOL variable type to parallel-netcdf type
+	 */
+	switch(vartype) {
+		case SMIOL_REAL32:
+			xtype = NC_FLOAT;
+			break;
+		case SMIOL_REAL64:
+			xtype = NC_DOUBLE;
+			break;
+		case SMIOL_INT32:
+			xtype = NC_INT;
+			break;
+		case SMIOL_CHAR:
+			xtype = NC_CHAR;
+			break;
+		default:
+			free(dimids);
+			return SMIOL_INVALID_ARGUMENT;
+	}
+
+	/*
+	 * Define the variable
+	 */
+	if ((ierr = ncmpi_def_var(file->ncidp, varname, xtype, ndims, dimids, &varidp)) != NC_NOERR) {
+		free(dimids);
+		file->context->lib_type = SMIOL_LIBRARY_PNETCDF;
+		file->context->lib_ierr = ierr;
+		return SMIOL_LIBRARY_ERROR;
+	}
+
+	free(dimids);
+#endif
 
 	return SMIOL_SUCCESS;
 }
